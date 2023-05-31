c58e331e1910038939b0568c5109863d0f701afb
vagrant <datsitsre>
2023-05-31 12:42:30 +0000

    Insertion at the left side

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -17,9 +17,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 	nodeInsert->n = value;
 	nodeInsert->parent = parent;
-	nodeInsert->right = NULL;
 	nodeInsert->left = parent->left;
-	nodeInsert->left = nodeInsert;
+	parent->left = nodeInsert;
+	nodeInsert->right = NULL;
 
 	if (nodeInsert->left)
 		nodeInsert->left->parent = nodeInsert;
