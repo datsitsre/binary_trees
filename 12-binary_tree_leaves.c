d5fcd023396c875e15a68bc37a3e21a415ff3dbe
vagrant <datsitsre>
2023-05-31 17:44:31 +0000

    12. Leaves

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - count number of leaves
+ * @tree: main tree
+ * Return: count of tree
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t count_leaves = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
+
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
+
+
